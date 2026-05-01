c29b15be2a8d0959a3467d2f7ddc790ce4838eb7
Chowdhury Amir Abdullah <abdullahamir52@gmail.com>
2026-05-02 04:41:32 +0600

    Initial
    Initial code for w3schools

---
+++ Python/w3Schools_lessons/002_Python_Syntax.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+    print('Five is greater than two!')
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+# This is a comment. This helps us know about the code,
+# without giving syntax error
